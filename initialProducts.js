eb67476c1a74031ce9373eb38a157bb807dab071
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-10-31 23:24:31 -0300

    first commit

---
+++ initialProducts.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+    title: 'Mouse',
+    price: 150,
+    thumbnail:
+      'https://st.depositphotos.com/1004915/2035/i/600/depositphotos_20353239-stock-photo-computer-mouse.jpg',
+  },
+  {
+    title: 'Teclado',
+    price: 180,
+    thumbnail:
+      'https://www.muycomputer.com/wp-content/uploads/2019/10/Microsoft_kyb.jpg',
+  },
+  {
+    title: 'Clip',
+    price: 18,
+    thumbnail:
+      'https://www.distritooficina.com/wp-content/uploads/2019/03/clip1.jpg',
+  },
+  {
+    title: 'Mousepad',
+    price: 35,
+    thumbnail:
+      'https://resource.logitechg.com/d_transparent.gif/content/dam/products/gaming/mice/g640-cloth-gaming-mouse-pad/g640-large-cloth-gaming-mouse-pad13.png',
+  },
+  {
+    title: 'Championes',
+    price: 3300,
+    thumbnail:
+      'https://resource.logitechg.com/d_transparent.gif/content/dam/products/gaming/mice/g640-cloth-gaming-mouse-pad/g640-large-cloth-gaming-mouse-pad13.png',
+  },
+  {
+    title: 'Magic Trackpad',
+    price: 169,
+    thumbnail:
+      'https://http2.mlstatic.com/D_NQ_NP_2X_673818-MLA46250587136_062021-F.webp',
+  },
+  {
+    title: 'Monitor Dell 27"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+  {
+    title: 'Monitor Dell 27"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+  {
+    title: 'Monitor Dell 27"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+  {
+    title: 'Monitor Dell 27"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+];
---
+++ script-to-json.js
@@ -0,0 +1,70 @@
+const jsArray = [
+  {
+    title: 'Mouse',
+    price: 150,
+    thumbnail:
+      'https://st.depositphotos.com/1004915/2035/i/600/depositphotos_20353239-stock-photo-computer-mouse.jpg',
+  },
+  {
+    title: 'Teclado',
+    price: 180,
+    thumbnail:
+      'https://www.muycomputer.com/wp-content/uploads/2019/10/Microsoft_kyb.jpg',
+  },
+  {
+    title: 'Clip',
+    price: 18,
+    thumbnail:
+      'https://www.distritooficina.com/wp-content/uploads/2019/03/clip1.jpg',
+  },
+  {
+    title: 'Mousepad',
+    price: 35,
+    thumbnail:
+      'https://resource.logitechg.com/d_transparent.gif/content/dam/products/gaming/mice/g640-cloth-gaming-mouse-pad/g640-large-cloth-gaming-mouse-pad13.png',
+  },
+  {
+    title: 'Championes',
+    price: 3300,
+    thumbnail:
+      'https://resource.logitechg.com/d_transparent.gif/content/dam/products/gaming/mice/g640-cloth-gaming-mouse-pad/g640-large-cloth-gaming-mouse-pad13.png',
+  },
+  {
+    title: 'Magic Trackpad',
+    price: 169,
+    thumbnail:
+      'https://http2.mlstatic.com/D_NQ_NP_2X_673818-MLA46250587136_062021-F.webp',
+  },
+  {
+    title: 'Monitor Dell 32"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+  {
+    title: 'Monitor Dell 27"',
+    price: 355,
+    thumbnail: 'http://imagenes.com/imagen-monitor.jpg',
+  },
+  {
+    title: 'Juego Call of Duty',
+    price: 120,
+    thumbnail:
+      'https://cdn.europosters.eu/image/750/posters/call-of-duty-black-ops-3-cover-i32831.jpg',
+  },
+  {
+    title: 'Juego GTA Vice City',
+    price: 120,
+    thumbnail:
+      'https://upload.wikimedia.org/wikipedia/en/c/ce/Vice-city-cover.jpg',
+  },
+  {
+    title: 'Juego GTA San Andreas',
+    price: 120,
+    thumbnail:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/GTASABOX.jpg/220px-GTASABOX.jpg',
+  },
+];
+
+const jsonArray = JSON.stringify(jsArray);
+console.log(jsonArray);
+console.log(jsArray.length);
